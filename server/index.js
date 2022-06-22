3df7726640652bcc99519ac2cee318518113b091
Eli Tabello <etabw3@gmail.com>
2022-06-22 17:52:18 -0400

    comment server endpoints

--- server/index.js
+++ server/index.js
@@ -12,11 +12,13 @@ app.use(cors())
 
 app.listen(3333, () => console.log('Express on 3333'))
 
-// 10 % francise fee
+// 10 % franchise fee
 const FEE = 0.1
 
+// helper to round and format currency
 const roundMoney = m => (Math.round(m * 100) / 100).toFixed(2)
 
+// return a sorted array based on last name
 app.get('/franchisees', async (req, res) => {
   try {
     const sorted = franchisees.sort((a,b) => {
@@ -30,6 +32,7 @@ app.get('/franchisees', async (req, res) => {
   }
 })
 
+// return dict of locations based on location id
 app.get('/locations', async (req, res) => {
   try {
     const entries = locations.map(l => [l._id, l.address])
@@ -39,7 +42,8 @@ app.get('/locations', async (req, res) => {
     res.sendStatus(http.BAD_REQUEST)
   }
 })
-
+// return totalSales, totalFee and a dict on location_id
+// each location also has fee and total sales calculated and rounded
 app.get('/sales', async (req, res) => {
   try {
     const data = await groupBy(sales, f => f.location_id)
