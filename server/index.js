199c88d6bb35d8e00cefb85963e0bd523424ba4f
Eli Tabello <etabw3@gmail.com>
2022-06-22 15:18:57 -0400

    initial endpoints grouped on location_id and francise_id

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const cors = require('cors')
+const express = require('express')
+const groupBy = require('lodash.groupby')
+const http = require('http-status-codes').StatusCodes
+
+const franchisees = require('./data/franchisees.json')
+const locations = require('./data/locations.json')
+const sales = require('./data/sales.json')
+
+const app = express()
+app.use(cors())
+
+app.listen(3333, () => console.log('Express on 3333'))
+
+app.get('/franchisees', async (req, res) => {
+  try {
+    const data = await groupBy(franchisees, f => f._id)
+    res.send(data)
+  } catch (e) {
+    res.sendStatus(http.BAD_REQUEST)
+  }
+})
+
+app.get('/locations', async (req, res) => {
+  try {
+    const entries = locations.map(l => [l._id, l.address])
+    const data = Object.fromEntries(entries)
+    res.send(data)
+  } catch (e) {
+    res.sendStatus(http.BAD_REQUEST)
+  }
+})
+
+app.get('/sales', async (req, res) => {
+  try {
+    const data = await groupBy(sales, f => f.location_id)
+    res.send(data)
+  } catch (e) {
+    res.sendStatus(http.BAD_REQUEST)
+  }
+})
